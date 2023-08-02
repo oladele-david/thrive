ce7f8a90ccd63940d0ce786d3c4b03aa411e5fe7
oladele.david <tdboy59@gmail.com>
2023-08-02 02:12:36 +0100

    Made modifications

--- classes/Savings.class.php
+++ classes/Savings.class.php
@@ -11,24 +11,32 @@ class Savings
         $this->pdo = Database::connect();
     }
 
-    public function createSaving($accountId, $amount, $minimumAmount, $savingInterval, $startDate)
+    public function createSaving($accountId, $amount, $minimumAmount, $savingInterval, $startDate, $duration, $special)
     {
         try {
-            
-            $activeSaving = $this->getActiveSaving($accountId);
+
+            $activeSaving = $this->getActiveSaving($accountId, $special);
             if ($activeSaving) {
-                // If there is an active saving, return an error message
-                $value_return = array("response" => "error", "title" => "Oops!", "msg" => "You already have an active saving.");
-                return json_encode($value_return);
+                if ($special == true) {
+                    // If there is an active saving, return an error message
+                    $value_return = array("response" => "error", "title" => "Oops!", "msg" => "You already have an active special saving.".$special);
+                    return json_encode($value_return);
+                } else {
+                    // If there is an active saving, return an error message
+                    $value_return = array("response" => "error", "title" => "Oops!", "msg" => "You already have an active saving.".$special);
+                    return json_encode($value_return);
+                }
+                
+               
             }
-    
+
             // Check if the amount to save is greater than or equal to the minimum amount
             if ($amount < $minimumAmount) {
                 // If the amount is less than the minimum, return an error message
                 $value_return = array("response" => "error", "title" => "Oops!", "msg" => "Amount to save cannot be less than the minimum required amount.");
                 return json_encode($value_return);
             }
-    
+
             $account = new Account();
             $accountBalance = $account->getAccountBalance($accountId);
 
@@ -45,38 +53,44 @@ class Savings
                 return json_encode($value_return);
             }
 
+            // Prepare SQL statement to insert a new saving into the database
+            $stmt = $this->pdo->prepare(
+                "INSERT INTO tb_savings (ref_no, account_id, amount, saving_interval, start_date, ending_date, special, minimum_amount, next_removing_date) 
+                                         VALUES (:refNo, :accountId, :amount, :savingInterval, :startDate, :endingDate, :special, :minimumAmount, :nextRemovingDate)"
+            );
+            
             // Generate the refNo using time()
             $refNo = time();
 
-            // Prepare SQL statement to insert a new saving into the database
-            $stmt = $this->pdo->prepare("INSERT INTO tb_savings (ref_no, account_id, amount, saving_interval, start_date, minimum_amount, next_removing_date) 
-                                         VALUES (:refNo, :accountId, :amount, :savingInterval, :startDate, :minimumAmount, :nextRemovingDate)");
-    
             // Calculate the next removing date based on the saving_interval and start_date
             $nextRemovingDate = Custom::calculateNextDate($savingInterval, $startDate);
-    
+
+            $endingDate = date('Y-m-d', strtotime($startDate . " + $duration months"));
+
             // Bind the saving inputs to the placeholders in the SQL statement
             $stmt->bindParam(':refNo', $refNo);
             $stmt->bindParam(':accountId', $accountId);
             $stmt->bindParam(':amount', $amount);
             $stmt->bindParam(':savingInterval', $savingInterval);
             $stmt->bindParam(':startDate', $startDate);
+            $stmt->bindParam(':endingDate', $endingDate); // Bind the endingDate parameter
+            $stmt->bindParam(':special', $special, PDO::PARAM_BOOL);
             $stmt->bindParam(':minimumAmount', $minimumAmount);
             $stmt->bindParam(':nextRemovingDate', $nextRemovingDate);
-    
+
             // Execute the SQL statement to create a new saving
             $stmt->execute();
-    
+
             // Get the ID of the newly inserted saving (assuming you have an auto-incrementing primary key column)
             $newSavingId = $this->pdo->lastInsertId();
-    
+
             // Call the addSavingsHistory method to add the savings history record
             $this->addSavingsHistory($accountId, $newSavingId, $amount);
-    
+
             // Deduct the amount from the account balance
             $account = new Account();
             $account->deductBalance($accountId, $amount);
-    
+
             // If successful, return a success message
             $value_return = array("response" => "success", "title" => "Success!", "msg" => "Saving created successfully.");
             return json_encode($value_return);
@@ -86,7 +100,7 @@ class Savings
             return json_encode($value_return);
         }
     }
-    
+
 
     public function updateSaving($savingId, $amount, $accountId)
     {
@@ -115,7 +129,7 @@ class Savings
                 $value_return = array("response" => "error", "title" => "Oops!", "msg" => "Amount to save cannot be less than the minimum amount.");
                 return json_encode($value_return);
             }
-            
+
             if ($amount > $accountBalance) {
                 // If the amount to save is greater than the account balance, return an error message
                 $value_return = array("response" => "error", "title" => "Oops!", "msg" => "Amount to save cannot be greater than the account balance.");
@@ -134,6 +148,7 @@ class Savings
             $stmt->execute();
 
             $this->addSavingsHistory($accountId, $savingId, $amount);
+            $account->deductBalance($accountId, $amount);
 
             // If successful, return a success message
             $value_return = array("response" => "success", "title" => "Success!", "msg" => "Saving updated successfully.");
@@ -168,14 +183,49 @@ class Savings
         }
     }
 
-    public function getActiveSaving($accountId)
+    public function getSaving($savingId)
     {
         try {
+            // Prepare SQL statement to retrieve the saving history for the specified account
+            $stmt = $this->pdo->prepare("SELECT * FROM tb_savings WHERE id = :savingId");
+
+            // Bind the account ID to the placeholder in the SQL statement
+            $stmt->bindParam(':savingId', $savingId);
+
+            // Execute the SQL statement
+            $stmt->execute();
+
+            // Fetch the saving history records
+            $saving = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // Return the saving history
+            return $saving;
+        } catch (PDOException $e) {
+            // If there is an error, return false
+            return false;
+        }
+    }
+
+    public function getActiveSaving($accountId, $special = null)
+    {
+        try {
+            $query = "SELECT * FROM tb_savings WHERE account_id = :accountId AND status = 'active'";
+
+            // If $special is provided and is a boolean value, add the condition for the 'special' column
+            if (isset($special)) {
+                $query .= " AND special = :special";
+            }
+
             // Prepare SQL statement to retrieve the active saving for the specified account
-            $stmt = $this->pdo->prepare("SELECT * FROM tb_savings WHERE account_id = :accountId AND status = 'active'");
+            $stmt = $this->pdo->prepare($query);
 
             // Bind the account ID to the placeholder in the SQL statement
             $stmt->bindParam(':accountId', $accountId);
+
+            // If $special is provided and is a boolean value, bind the 'special' parameter
+            if (isset($special)) {
+                $stmt->bindParam(':special', $special, PDO::PARAM_BOOL);
+            }
 
             // Execute the SQL statement
             $stmt->execute();
